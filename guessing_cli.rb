e4d7547f0c079bab0485528fc16972d5bafb7802
Rosa Tu <rosalina.tu@gmail.com>
2018-07-15 03:57:16 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,9 @@
+require 'pry'
 def run_guessing_game
   print "Guess a number between 1 and 6"
   user_in = gets.to_i
   computer_in = rand(6) 
+  binding.pry
   until user_in == computer_in
   if user_in == "exit"
     puts "Goodbye!"
